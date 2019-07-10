63c47d40cf745abc4bfbb74246f5dc8697a619aa
Denislav Ganchev <denislav.ganchev@mentormate.com>
2019-07-10 16:46:43 +0300

    test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Simple message</p>
+        <p>Simple message</p>
       </header>
     </div>
   );
